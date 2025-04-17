fbef6296b0329dae432597057f96b546973936ef
hechima256 <hechima11444@gmail.com>
2025-04-17 18:38:21 +0900

    テストの日本語化

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,32 +3,24 @@ import userEvent from '@testing-library/user-event';
 import { describe, it, expect } from 'vitest';
 import App from './App';
 
-describe('App', () => {
-	it('renders Vite + React heading', () => {
+describe('トップ画面', () => {
+	it('Vite + Reactのタイトルが表示されること', () => {
 		render(<App />);
 		const heading = screen.getByText('Vite + React');
 		expect(heading).toBeInTheDocument();
 	});
 
-	it('renders count button with initial value of 0', () => {
+	it('カウンターの最初の値が0であること', () => {
 		render(<App />);
 		const countButton = screen.getByText('count is 0');
 		expect(countButton).toBeInTheDocument();
 	});
 
-	it('increments count when button is clicked', async () => {
+	it('カウンターをクリックすると値が1増えること', async () => {
 		render(<App />);
 		const countButton = screen.getByText('count is 0');
-		// await userEvent.click(countButton);
-		fireEvent.click(countButton);
-		expect(screen.getByText('count is 1')).toBeInTheDocument();
-	});
+		await userEvent.click(countButton);
 
-	it('renders Vite and React logos', () => {
-		render(<App />);
-		const viteLogo = screen.getByAltText('Vite logo');
-		const reactLogo = screen.getByAltText('React logo');
-		expect(viteLogo).toBeInTheDocument();
-		expect(reactLogo).toBeInTheDocument();
+		expect(screen.getByText('count is 1')).toBeInTheDocument();
 	});
 });
