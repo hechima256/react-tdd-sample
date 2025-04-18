70a3b567a21cacd69b85182c443522a0ea6f5db4
hechima256 <hechima11444@gmail.com>
2025-04-18 23:36:57 +0900

    テスト環境でモックを使えるようにした

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,10 +1,19 @@
 import '@testing-library/jest-dom';
-import { expect, afterEach } from 'vitest';
+import { expect, afterEach, beforeAll, afterAll } from 'vitest';
 import { cleanup } from '@testing-library/react';
 import * as matchers from '@testing-library/jest-dom/matchers';
+import { server } from '../mocks/node';
 
 expect.extend(matchers);
 
+beforeAll(() => {
+	server.listen();
+});
+afterAll(() => {
+	server.close();
+});
+
 afterEach(() => {
+	server.resetHandlers();
 	cleanup();
 });
