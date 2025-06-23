3afea2fdab89b61bb3fbd1b2ee5e61301494eddc
hechima256 <hechima11444@gmail.com>
2025-06-23 22:11:15 +0900

    feat: SWRを使用してAPIデータの取得を最適化し、環境変数ファイルサンプルを追加

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,49 +1,18 @@
-import { ResourceResponse } from '@react-tdd-sample/shared/types';
-import { useEffect, useState } from 'react';
+import { ResourceResponse } from '@react-tdd-sample/shared/src/types/api';
+import useSWR from 'swr';
+
 import './App.css';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 
 function App() {
-	const [count, setCount] = useState(0);
-	const [apiResponse, setApiResponse] = useState<string>('');
+	const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
+	const fetcher = (url: string) => fetch(`${apiBaseUrl}${url}`).then(res => res.json());
+	const { data, error } = useSWR<ResourceResponse>('/resource', fetcher);
 
-	useEffect(() => {
-		const fetchData = async () => {
-			try {
-				const response = await fetch('/resource');
-				const data: ResourceResponse = await response.json();
-				setApiResponse(data.text);
-			} catch (error) {
-				console.error('Error fetching data:', error);
-				setApiResponse('エラーが発生しました');
-			}
-		};
-
-		fetchData();
-	}, []);
 	return (
 		<>
-			<div>
-				<a href="https://vite.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
 			<div className="card">
-				<button onClick={() => setCount(count => count + 1)}>count is {count}</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-				<div className="api-response">
-					<h2>API Response:</h2>
-					<pre>{apiResponse}</pre>
-				</div>
+				<pre>{error ? 'エラーが発生しました' : data ? data.text : 'Loading...'}</pre>
 			</div>
-			<p className="read-the-docs">Click on the Vite and React logos to learn more</p>
 		</>
 	);
 }
