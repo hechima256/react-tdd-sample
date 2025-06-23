9b92d206acfabefcc727ef1c1c7eef3836ab5f03
hechima256 <hechima11444@gmail.com>
2025-06-23 22:11:35 +0900

    モックのAPIを削除

--- packages/frontend/src/mocks/handlers.ts
+++ packages/frontend/src/mocks/handlers.ts
@@ -1,8 +1,5 @@
-import { HttpResponse, http } from 'msw';
-import { ResourceResponse } from '../types/api';
-
 export const handlers = [
-	http.get('/resource', () => {
-		return HttpResponse.json<ResourceResponse>({ text: 'Hello World' });
-	}),
+	// http.get('/resource', () => {
+	// 	return HttpResponse.json<ResourceResponse>({ text: 'Hello World' });
+	// }),
 ];
