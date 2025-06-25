ba4c5986d2a5089d3dfcb87b1f09a2b2cade8aff
hechima256 <hechima11444@gmail.com>
2025-06-25 23:32:28 +0900

    test: /todosエンドポイントのテストをリファクタリングし、beforeEachを追加

--- packages/backend/src/index.test.ts
+++ packages/backend/src/index.test.ts
@@ -1,41 +1,39 @@
-import { describe, expect, it } from 'vitest';
+import type { FastifyInstance } from 'fastify';
+import { beforeEach, describe, expect, it } from 'vitest';
 import { buildServer } from './index';
 
 describe('GET /todos', () => {
-	it('should return 200', async () => {
-		const app = await buildServer();
-		const response = await app.inject({
+	let app: FastifyInstance;
+
+	beforeEach(async () => {
+		app = await buildServer();
+	});
+
+	const getTodos = async () => {
+		return await app.inject({
 			method: 'GET',
 			url: '/todos',
 		});
+	};
+
+	it('should return 200', async () => {
+		const response = await getTodos();
 		expect(response.statusCode).toBe(200);
 	});
 
 	it('should return JSON', async () => {
-		const app = await buildServer();
-		const response = await app.inject({
-			method: 'GET',
-			url: '/todos',
-		});
+		const response = await getTodos();
 		expect(response.headers['content-type']).toContain('application/json');
 	});
 
 	it('should return an array', async () => {
-		const app = await buildServer();
-		const response = await app.inject({
-			method: 'GET',
-			url: '/todos',
-		});
+		const response = await getTodos();
 		const todos = JSON.parse(response.payload);
 		expect(todos).toBeInstanceOf(Array);
 	});
 
 	it('should return an empty array (initial state)', async () => {
-		const app = await buildServer();
-		const response = await app.inject({
-			method: 'GET',
-			url: '/todos',
-		});
+		const response = await getTodos();
 		const todos = JSON.parse(response.payload);
 		expect(todos).toEqual([]);
 	});
