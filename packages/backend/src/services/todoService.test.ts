544b1f1abe722c1ca8ce0ba4951515805ebf44aa
hechima256 <hechima11444@gmail.com>
2025-07-02 21:55:08 +0900

    インメモリDBのコンストラクタのデフォルト引数を廃止し、DIに変更

--- packages/backend/src/services/todoService.test.ts
+++ packages/backend/src/services/todoService.test.ts
@@ -1,11 +1,18 @@
 import { beforeEach, describe, expect, it } from 'vitest';
 import { InMemoryTodoDB, TodoService, type Todo } from './todoService';
 
+// テスト用のデータ
+const TEST_DATA: Todo[] = [
+	{ id: 1, title: 'test1', completed: false },
+	{ id: 2, title: 'test2', completed: false },
+	{ id: 3, title: 'test3', completed: false },
+];
+
 // テスト専用のTodoService拡張
 class TestableTodoService extends TodoService {
 	private testDb: InMemoryTodoDB;
 	constructor() {
-		const db = new InMemoryTodoDB();
+		const db = new InMemoryTodoDB(TEST_DATA);
 		super(db);
 		this.testDb = db;
 	}
@@ -22,11 +29,7 @@ describe('TodoService (GET)', () => {
 	});
 
 	it('return all data initialized', () => {
-		expect(service.getAllTodos()).toEqual([
-			{ id: 1, title: 'test1', completed: false },
-			{ id: 2, title: 'test2', completed: false },
-			{ id: 3, title: 'test3', completed: false },
-		]);
+		expect(service.getAllTodos()).toEqual(TEST_DATA);
 	});
 
 	it('return set data', () => {
