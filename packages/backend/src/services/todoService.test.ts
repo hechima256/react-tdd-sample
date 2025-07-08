94e5650b628616553ef110e66fbf56555992b632
hechima256 <hechima11444@gmail.com>
2025-07-08 22:14:45 +0900

    3Aがわかりやすいようにテストを整理

--- packages/backend/src/services/todoService.test.ts
+++ packages/backend/src/services/todoService.test.ts
@@ -14,7 +14,9 @@ describe('TodoService (GET)', () => {
 		const testData = [{ id: 1, title: 'test', completed: false }];
 		testDb.setAll(testData);
 
-		expect(service.getAllTodos()).toEqual(testData);
+		const result = service.getAllTodos();
+
+		expect(result).toEqual(testData);
 	});
 });
 
@@ -29,6 +31,7 @@ describe('TodoService (POST)', () => {
 
 	it('add new todo to empty todos', () => {
 		const newTodo = { id: 1, title: 'test', completed: false };
+
 		service.addTodo(newTodo);
 
 		expect(service.getAllTodos()).toEqual([newTodo]);
@@ -37,8 +40,8 @@ describe('TodoService (POST)', () => {
 	it('add new todo to existing todos', () => {
 		const initialTodo = { id: 1, title: 'test', completed: false };
 		testDb.setAll([initialTodo]);
-
 		const newTodo = { id: 2, title: 'test2', completed: false };
+
 		service.addTodo(newTodo);
 
 		expect(service.getAllTodos()).toEqual([initialTodo, newTodo]);
